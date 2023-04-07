2240558952798fa7ad2a87381fb5aee6884d1f8a
John Do <johndo0277@icloud.com>
2023-04-06 23:01:31 -0500

    Display Genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,26 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageURL from "../serivices/image-url";
+
+const GenreList = () => {
+  const { data } = useGenres();
+
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageURL(genre.image_background)}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
