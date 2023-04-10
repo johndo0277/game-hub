713647ddb45c268ca9ce996a06c23fd281e03b8d
John Do <johndo0277@icloud.com>
2023-04-10 14:24:13 -0500

    Build static data platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,13 @@
-import useData from "./useData";
+import platform from "../data/platform";
 
 interface Platform {
     id: number;
     name: string;
     slug: string;
 }
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+//const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => (
+    { data: platform, isLoading: false, error: null }
+);
 
 export default usePlatforms;
