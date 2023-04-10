7e51fbefdc2985619b62ac04ad9a735fd57b527d
John Do <johndo0277@icloud.com>
2023-04-09 21:24:22 -0500

    Implement searching

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import theme from "./theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
