ca504a5a747d0ffae31e99b78189611250902c81
John Do <johndo0277@icloud.com>
2023-04-10 09:58:46 -0500

    guide for loading genres file

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -10,7 +10,7 @@ export interface Genre {
 //because of user's experience . We dont want to load Genre adn Game at the same time
 //we make a genre.ts to store a static data
 //--> load genres from server ->> const useGenres = () => useData<Genre>('/genres');
-//--> make a static Genres data by modifying the useGenres()
+//--> make a static Genres data by modifying the useGenres() and loading static data file
 const useGenres = () => (
     { data: genres , isLoading: false, error: null }
 
